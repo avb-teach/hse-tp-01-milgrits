35bdd47c9635a4b50ed8de69285283c5306e7bd0
Oleg Ushakov <ooushakov@edu.hse.ru>
2025-04-27 18:59:54 +0300

    jjj

--- collect_files.sh
+++ collect_files.sh
@@ -58,17 +58,24 @@ for root, dirs, files in os.walk(input_dir):
     depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1
 
     if max_depth > 0 and depth > max_depth:
-        dirs[:] = []  # Don't walk deeper
+        dirs.clear()  # Don't go deeper
         continue
+
+    if rel_path != ".":
+        dest_dir = os.path.join(output_dir, rel_path)
+    else:
+        dest_dir = output_dir
+
+    os.makedirs(dest_dir, exist_ok=True)
 
     for file in files:
         src_file = os.path.join(root, file)
-        dest_file = os.path.join(output_dir, file)
+        dest_file = os.path.join(dest_dir, file)
 
         base, ext = os.path.splitext(file)
         counter = 1
         while os.path.exists(dest_file):
-            dest_file = os.path.join(output_dir, f"{base}_{counter}{ext}")
+            dest_file = os.path.join(dest_dir, f"{base}_{counter}{ext}")
             counter += 1
 
         shutil.copy2(src_file, dest_file)
